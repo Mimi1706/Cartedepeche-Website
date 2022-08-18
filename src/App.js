f93b246606a80ec110425dd50e394bdd8f9204c6
Mimi <87867561+Mimi1706@users.noreply.github.com>
2022-08-18 20:59:43 +0200

    HomePage update + Routes added

---
+++ src/App.js
@@ -0,0 +1,23 @@
+import "./App.css"
+
+import Header from "./components/Header/Header"
+
+import { BrowserRouter, Routes, Route } from "react-router-dom"
+import Home from "./pages/Home/Home"
+import Footer from "./components/Footer/Footer"
+
+function App() {
+  return (
+    <div className="App">
+      <BrowserRouter>
+        <Header />
+        <Routes>
+          <Route path="/" element={<Home />}></Route>
+        </Routes>
+        <Footer />
+      </BrowserRouter>
+    </div>
+  )
+}
+
+export default App
